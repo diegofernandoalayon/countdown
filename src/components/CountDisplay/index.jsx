685c08d302b2e38f362638dda1777a06b7b03044
Diego Fernando Alayon <dfar.41@gmail.com>
2022-04-26 23:44:37 -0500

    creando componente CountDisplay, para mostrar la cuenta atras, estilos

---
+++ src/components/CountDisplay/index.jsx
@@ -0,0 +1,26 @@
+import './countDisplay.css'
+export default function CountDisplay({timerDays, timerHours, timerMinutes, timerSeconds}){
+  return(
+    <div className="display">
+      <div>
+        <p>{timerDays}</p>
+        <p><small>Days</small></p>
+      </div>
+      <span>:</span>
+      <div>
+        <p>{timerHours}</p>
+        <p><small>Hours</small></p>
+      </div>
+      <span>:</span>
+      <div>
+        <p>{timerMinutes}</p>
+        <p><small>Minutes</small></p>
+      </div>
+      <span>:</span>
+      <div>
+        <p>{timerSeconds}</p>
+        <p><small>seconds</small></p>
+      </div>
+    </div>
+  )
+}
