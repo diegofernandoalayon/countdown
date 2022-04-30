2f26c62eb77c311e84a17b4acc4fcd553568528b
Diego Fernando Alayon <dfar.41@gmail.com>
2022-04-29 23:56:36 -0500

    probando agregar fecha en componente FormTime

--- src/components/FormTime/index.jsx
+++ src/components/FormTime/index.jsx
@@ -1,12 +1,17 @@
-export default function FormTime(){
+export default function FormTime({setTimerCreated}){
   const handleSubmit = (event) =>{
     event.preventDefault()
+    // setTimerCreated(true)
+    console.log(event.target.value)
+  }
+  const handleChange = (event) =>{
+    console.log(event.target.value)
   }
   return (
     <>
       <h2>set Time</h2>
       <form onSubmit={handleSubmit}>
-        <input type="number" />
+        <input type="date" onChange={handleChange} required/>
         <button>establecer</button>
       </form>
     </>
