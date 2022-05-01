a7ca3208484611cab80afe331f6c60c1ed5213d6
Diego Fernando Alayon <dfar.41@gmail.com>
2022-04-30 23:57:40 -0500

    simplificando punto de entrada de la app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import { useState } from 'react'
 
+
 import './App.css'
+import CountDown from './components/Countdown'
+import FormTime from './components/FormTime'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [timerCreated, setTimerCreated] = useState(true)
+ 
   return (
     <article className="App">
       <section className="container">
         <h1>CountDown</h1>
+        <div className='countdown'>
+          {
+            timerCreated ? <CountDown /> : <FormTime setTimerCreated={setTimerCreated}/>
+          }
+        </div>
+        
       </section>
     </article>
   )
